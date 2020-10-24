228a0e34861deaa874327c1a4943e11c73d16447
Dylan Sapienza <dylsapienza12@gmail.com>
2020-10-24 11:50:49 -0400

    Added Comments and Ternary Leading Zero

--- client/pomodoro-timer/src/components/Timer.js
+++ client/pomodoro-timer/src/components/Timer.js
@@ -22,10 +22,14 @@ import "@ionic/core/css/ionic.bundle.css";
 
 function Timer(props){
 
+    //State Variables
+
     const [mins, setMins] = useState(0);
     const [secs, setSecs] = useState(5);
     const [isRunning, setIsRunning] = useState(false)
     const [cycles, setCycles] = useState(0)
+
+    //Reset Button - Sets to Pomodoro, 0 Cycles and Pauses
 
     function reset(){
         setCycles(0)
@@ -34,10 +38,15 @@ function Timer(props){
         setIsRunning(false)
     }
 
+    //If we use all out seconds, set to 59 and reduce the mins
+
     if(secs < 0){
         setSecs(59)
         setMins(mins - 1)
     }
+
+    //If we are out of mins then initiate beep and begin the next cycle
+    //Depending on cycle count, set time and begin running
 
     if(mins < 0){
         console.log("BEEP BEEP");
@@ -59,12 +68,9 @@ function Timer(props){
             setIsRunning(true)
         }
         
-        // if (cycles % 5 == 0){
-        //     setMins(15)
-        //     setIsRunning(true)
-        // }
     }
 
+    //Countdown Tick-Tock
     useEffect(() => {
         if (isRunning){
             const id = window.setInterval(()=>{
@@ -76,7 +82,8 @@ function Timer(props){
 
     return(
         <>
-        <p color="dark" style={{ color: "black", fontSize: 90, fontWeight: 600, textAlign: "center"}}>{mins}:{secs}</p>
+        {/* Ternary Operator Inserts Leading Zero For Timer */}
+        <p color="dark" style={{ color: "black", fontSize: 90, fontWeight: 600, textAlign: "center"}}>{mins}:{secs < 10 ? "0": null}{secs} </p> 
         <button onClick={() => setIsRunning(false)}> Pause</button>
         <button onClick={() => setIsRunning(true)}> Play</button>
         <button onClick={() => reset()}> Reset</button>
